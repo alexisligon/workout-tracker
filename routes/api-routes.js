43f4045371b17d9ae7d2079972ff0f01108cb96c
Lex <aligon17@gmail.com>
2021-08-04 14:37:01 -0400

    get all api workouts works

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router(); 
 const db = require('../models'); 
 
+// get all workouts
 router.get('/api/workouts', (req, res) => {
     db.Workout.find({})
     .then(dbWorkout => { res.json(dbWorkout); })
