f85d956b93c80d0ff7c871dec4fbb501cd67efb5
Lex <aligon17@gmail.com>
2021-08-04 11:55:28 -0400

    deleted comments

--- models/workout.js
+++ models/workout.js
@@ -22,25 +22,3 @@ const workoutSchema = new Schema({
 const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
-
-// const Schema = mongoose.Schema;
-
-// const transactionSchema = new Schema({
-//   name: {
-//     type: String,
-//     trim: true,
-//     required: "Enter a name for transaction"
-//   },
-//   value: {
-//     type: Number,
-//     required: "Enter an amount"
-//   },
-//   date: {
-//     type: Date,
-//     default: Date.now
-//   }
-// });
-
-// const Transaction = mongoose.model("Transaction", transactionSchema);
-
-// module.exports = Transaction;
