c090bbd19e27cc6442bea07056c69753af3d2db2
Lex <aligon17@gmail.com>
2021-08-04 11:56:38 -0400

    added comments to html routes

--- routes/html/index.js
+++ routes/html/index.js
@@ -1,14 +1,17 @@
 const router = require('express').Router();
 const path = require('path');
 
+// gets index.html for landing page
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
 })
 
+// get stats.html
 router.get('/stats', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/stats.html'));
 })
 
+// get exercise.html
 router.get('/exercise', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/exercise.html'))
 })
