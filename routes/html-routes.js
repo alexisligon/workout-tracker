957b9d7f24ce8e743c59adb505b3d9df5cf5b38c
Lex <aligon17@gmail.com>
2021-08-04 14:34:54 -0400

    updated file paths for html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,17 +3,17 @@ const path = require('path');
 
 // gets index.html for landing page
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 })
 
 // get stats.html
 router.get('/stats', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/stats.html'));
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
 })
 
 // get exercise.html
 router.get('/exercise', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/exercise.html'))
+    res.sendFile(path.join(__dirname, '../public/exercise.html'))
 })
 
 
