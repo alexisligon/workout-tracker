6c357006b9f1656638abe58ebb9c22e5a750ee59
Lex <aligon17@gmail.com>
2021-08-05 18:10:56 -0400

    added forward slash for queries, deleted console log, get post and put are now working

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -10,31 +10,33 @@ router.get('/api/workouts', (req, res) => {
 });
 
 // post a new workout
-router.post('api/workouts/', ({ body }, res) => {
+router.post('/api/workouts/', ({ body }, res) => {
     db.Workout.create(body)
         .then(dbWorkout => { res.json(dbWorkout) })
         .catch(err => { res.json(err) });
 })
 
-router.put('api/workouts/:id', (req, res) => {
+// update a workout
+router.put('/api/workouts/:id', (req, res) => {
     db.Workout.findByIdAndUpdate(
         { _id: req.params.id },
         { $push: { exercises: req.body } },
         { new: true, runValidators: true })
         .then(dbWorkout => { res.json(dbWorkout); })
-        console.log(dbWorkout)
         .catch(err => res.json(err))
 })
 
 // get for the range
 
-// router.get('api/workouts/range', (req, res) => {
-//     db.Workout.find({})
-//         // .then(dbWorkout => {
-//         //     const workouts = dbWorkout.map(workout =>
-//         //          { const duration = workout.exercises.reduce(acc, next => {return acc + next.duration}) }
-//         //     0)
-//         // })
-// })
+router.get('/api/workouts/range', (req, res) => {
+    db.Workout.aggregate(
+        [
+            {
+              $addFields: { totalDuration: 'something'}
+              // add total duration from all exercises here????
+              }
+          ] 
+    )
+})
 
 module.exports = router;
